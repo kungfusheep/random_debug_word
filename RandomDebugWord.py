633f8b24790fb12f51254ee900fee51da3b5b107
Pete Griffiths <kungfusheep@users.noreply.github.com>
2023-05-21 23:04:35 +0100

    increase number of syllables

--- RandomDebugWord.py
+++ RandomDebugWord.py
@@ -6,14 +6,29 @@ import random
 # used as a debug print statement in Go, but could and probably will be
 # extended to other languages. (Or I find a better way of inserting the word)
 
-syllables_2 = ['ka', 'ki', 'ku', 'ke', 'ko', 'na', 'ni', 'nu', 'ne', 'no',
-               'sa', 'su', 'se', 'so', 'ta', 'te', 'to', 'ha', 'hi', 
-               'fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 
-               'yo', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wo']
+# The idea is that you can bind this to a key combination and then insert a quick
+# debug print statement prefixed with a random word at a fixed length. This is useful when printf 
+# debugging and you want to see the flow of your program. Instead of using random characters
+# to build the word, I use a list of Japanese syllables. I didn't want to use words 
+# because I don't want to get distracted by the meaning of the word, but I wanted 
+# something that was easy to read and pronounce. I chose Japanese because it has a 
+# limited set of syllables that work well when combined. I also think it is easier 
+# to read than the random characters I was using before. 
+
+syllables_2 = ['ka', 'ki', 'ku', 'ke', 'ko', 'ga', 'gi', 'gu', 'ge', 'go',
+                       'sa', 'su', 'se', 'so', 'za', 'ji', 'zu', 'ze', 'zo',
+                       'ta', 'te', 'to', 'da', 'de', 'do', 'na', 'ni', 'nu', 
+                       'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho', 'ba', 'bi', 'bu', 
+                       'be', 'bo', 'pa', 'pi', 'pu', 'pe', 'po', 'ma', 'mi', 'mu', 
+                       'me', 'mo', 'ya', 'yu', 'yo', 'ra', 'ri', 'ru', 're', 'ro', 
+                       'wa', 'wo']
 
 syllables_3 = ['shi', 'tsu', 'chi', 'nai', 'nei', 'nou', 'sai', 'sei', 'sou',
-               'tai', 'tei', 'tou', 'hai', 'hei', 'hou', 'mai', 'mei', 'mou', 
-               'yai', 'you', 'rai', 'rei', 'rou', 'wai', 'wou']
+               'tai', 'tei', 'tou', 'hai', 'hei', 'hou', 'mai', 'mei', 'mou',
+               'yai', 'you', 'rai', 'rei', 'rou', 'wai', 'wou',
+               'jya', 'jyu', 'jyo', 'gya', 'gyu', 'gyo', 'nya', 'nyu', 'nyo',
+               'hya', 'hyu', 'hyo', 'bya', 'byu', 'byo', 'pya', 'pyu', 'pyo',
+               'mya', 'myu', 'myo', 'rya', 'ryu', 'ryo']
 
 
 def get_word():
